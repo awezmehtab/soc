4095acd2b2b460683f3bb15495bbd5873cc29932
awezmehtab <shaikarifa178@gmail.com>
2024-07-01 21:05:00 +0530

    w5_q3 done

---
+++ week_5/w5_q1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    cin >> s;
+    ll n = s.size(), cnt = 0, inc = 1, l=0, r=0;
+    
+}
