bce4fd3e5843c83b5bb6d97b739aebf04156820d
Awez <114585074+awezmehtab@users.noreply.github.com>
2024-07-10 20:46:53 +0000

    codespaces op

--- week_7/w7_q3.cpp
+++ week_7/w7_q3.cpp
@@ -5,4 +5,6 @@ typedef long long ll;
 
 int main () {
     cout << "code likhna bacha hai bhaiya!\n";
+    cout << "yeh toh web me bhi kaam karta hai. ";
+    cout << "sab linux containers ka khel hai bhaiya!\n";
 }
